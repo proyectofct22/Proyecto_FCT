57ed7c9d195b590788f79a6cb325dbe968835814
proyectofct22 <115216986+proyectofct22@users.noreply.github.com>
2022-11-02 08:36:10 +0100

    Delete MVC_PLANTILLA directory

--- MVC_PLANTILLA/controllers/Controllers_CerrarSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie ("PHPSESSID", "", time() - 3600);
-header("Location:../index.php");
-?>
--- MVC_PLANTILLA/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	//setcookie('PHPSESSID', '', time() - 86400, '/');
-
-	require_once "./Databases/Database.php";
-	
-	require_once "./Controllers/Controller_Login.php";
-
-?>
