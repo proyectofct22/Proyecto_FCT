e93aa0b9f66e359ba73133b8cefc11c3bdf65a65
proyectofct22 <115216986+proyectofct22@users.noreply.github.com>
2022-12-13 19:57:39 +0100

    Delete V5 directory

--- V5/Controllers/Controller_Inicio.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	// Llamada a los documentos que componen la vista
-	require_once "../Views/Header.php";
-	require_once "../Views/Menu_Sin_Registrar.php";
-	require_once "../Views/View_Inicio.php";
-	require_once "../Views/Footer.php";
-
-?>
--- V5/Views/Menu_Login.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-dark border-bottom border-white fixed-top fondoOscuro">
-	<div class="container-fluid justify-content-center">
-		<a class="navbar-brand fuentePersonalizada mx-5" href="../Controllers/Controller_Inicio.php">Gaming Room</a>
-	</div>
-</nav>
